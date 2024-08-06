05ae39ace7769b44a0daf2a6f807073aa66b5281
alexei <rpsher@bk.ru>
2024-08-06 16:53:19 +0300

    Default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import '~/assets/css/style.css';
+import Header from '~/components/HeaderMenu.vue';
+import {useAuthStore} from '../store/authStore';
+import {onBeforeMount, computed, ref} from 'vue';
+
+const authStore = useAuthStore();
+
+
+</script>
+<template>
+  <div class="flex flex-col justify-between h-[100vh]">
+    <Header :isAuthorized="authStore.isAuthorized" :authorizedUser="authStore.authorizedUser"/>
+    <NuxtPage/>
+    <FooterMenu/>
+  </div>
+</template>
+<style scoped>
+</style>
