bdcd0b7e958d2f2718866c397073d2b813a4b906
alexei <rpsher@bk.ru>
2024-08-06 16:50:23 +0300

    Admin index file

---
+++ pages/admin/dashboard/index.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import {useAuthStore} from '../../../store/authStore';
+import {definePageMeta} from "../../../.nuxt/imports";
+
+definePageMeta({
+  layout: 'admin',
+  middleware: ['nuxt-permissions'],
+  roles: ['ROLE_ADMIN'],
+  permissions: []
+});
+
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  to: '/'
+}, {
+  label: 'Navigation',
+  icon: 'i-heroicons-square-3-stack-3d'
+}, {
+  label: 'Breadcrumb',
+  icon: 'i-heroicons-link'
+}]
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
