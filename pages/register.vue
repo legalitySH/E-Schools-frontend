da197d11f17b84499c2e684b52da41ba18eac48b
alexei <rpsher@bk.ru>
2024-08-06 16:52:22 +0300

    Registration page

---
+++ pages/register.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import RegistrationForm from "~/components/RegistrationForm.vue";
+</script>
+
+<template>
+  <RegistrationForm class="mt-10"/>
+</template>
+
+<style scoped>
+#register-header
+{
+  font-family: 'Prompt', sans-serif ;
+}
+</style>
